6e3c927e13e7232d87eadd33770137132b3695b8
davronkhamdamov <xamdamovdavron6@gmail.com>
2023-05-28 19:21:31 +0500

    page not found added

--- src/Router.jsx
+++ src/Router.jsx
@@ -8,14 +8,15 @@ import RootLayout from "./pages/layout/RootLayout.jsx";
 import Category from "./pages/Category/Category.jsx";
 import Login from "./pages/Login/Login.jsx";
 import Model_details from "./pages/ModelDetails/Model_details";
+import PageNotFound from "./pages/PageNotFound/PageNotFound";
 
 const rout = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<RootLayout />}>
-      <Route index element={<Category />}>
-        <Route path=":id" element={<Model_details />} />
-      </Route>
-      <Route path="login" element={<Login />} />
+      <Route index element={<Category />} />
+      <Route path="/cars/:id" element={<Model_details />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="*" element={<PageNotFound />} />
     </Route>
   )
 );
---
+++ src/pages/PageNotFound/PageNotFound.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import "./PageNotFound.css";
+import { Link } from "react-router-dom";
+
+const PageNotFound = () => {
+  return (
+    <div className="flex-container">
+      <div className="text-center">
+        <h1>
+          <span className="fade-in" id="digit1">
+            4
+          </span>
+          <span className="fade-in" id="digit2">
+            0
+          </span>
+          <span className="fade-in" id="digit3">
+            4
+          </span>
+        </h1>
+        <h3 className="fadeIn">PAGE NOT FOUND</h3>
+        <Link to="/" name="button">
+          Return To Home
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default PageNotFound;
