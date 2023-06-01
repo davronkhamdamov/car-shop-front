d292f64b31f38b573fb68c6f5edc03b8ff10bbf3
davronkhamdamov <xamdamovdavron6@gmail.com>
2023-06-01 21:13:45 +0500

    category added

--- src/pages/Category/Category.jsx
+++ src/pages/Category/Category.jsx
@@ -1,16 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Category.css";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import "react-lazy-load-image-component/src/effects/blur.css";
 
 const Category = () => {
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/photos")
+      .then((res) => res.json())
+      .then((data) => setData(data));
+  }, []);
+
   return (
     <div className="category_wrapper">
-      {new Array(8).fill("#").map(() => {
+      {data?.map((e) => {
         return (
-          <div className="category_item">
+          <div key={e.id} className="category_item">
             <div className="imgWrapper">
-              <img src="https://picsum.photos/400/250" alt="" />
+              <LazyLoadImage effect="blur" src={e.url} />
             </div>
-            <p className="category_title">CHEVROLET</p>
+            <p className="category_title">{e.title}</p>
           </div>
         );
       })}
