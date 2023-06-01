3bffd2569ae6a8573f83a986d8e2f5439e8edf65
davronkhamdamov <xamdamovdavron6@gmail.com>
2023-06-01 21:11:31 +0500

    header updated

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,21 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Header.css";
 import { Link, useLocation } from "react-router-dom";
 import { AiOutlineUser } from "react-icons/ai";
+import { BiLogIn } from "react-icons/bi";
+
 const Header = () => {
   const location = useLocation();
-  if (location.pathname === "/login") return;
+  const [isLogin, setIslogin] = useState(true);
+  if (["/login"].includes(location.pathname)) return;
   return (
     <div className="header">
-      <div className="headerLogo">
-        <Link to="/">GM</Link>
-      </div>
-      <div>
-        <Link to="/login" className="loginBtn">
-          <AiOutlineUser />
-          Admin o‘tish
-        </Link>
-      </div>
+      <>
+        <div className="headerLogo">
+          <Link to="/">GM</Link>
+        </div>
+        {!isLogin ? (
+          <div className="header_nav">
+            <Link to="/login" className="loginBtn">
+              <BiLogIn />
+              Register
+            </Link>
+            <Link to="/login" className="loginBtn">
+              <AiOutlineUser />
+              Login
+            </Link>
+          </div>
+        ) : (
+          <Link className="profile_img" to="/profile">
+            <img src="https://picsum.photos/60/60" alt="" />
+          </Link>
+        )}
+      </>
     </div>
   );
 };
