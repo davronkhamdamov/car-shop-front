8b84c724a497ba2637e95ec73af01fd8dc9815eb
davronkhamdamov <xamdamovdavron6@gmail.com>
2023-06-01 21:15:13 +0500

    container added

--- src/pages/layout/RootLayout.jsx
+++ src/pages/layout/RootLayout.jsx
@@ -1,14 +1,19 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Header from "../../components/Header/Header";
 import "./layout.css";
 
 const RootLayout = () => {
   return (
-    <div className="container">
-      <Header />
-      <Outlet />
-    </div>
+    <>
+      <div className="container">
+        <Header />
+      </div>
+      <div className="hr" />
+      <div className="container">
+        <Outlet />
+      </div>
+    </>
   );
 };
 export default RootLayout;
