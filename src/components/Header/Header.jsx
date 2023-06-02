e5366aef3a97eff72788d8afe59199beb1481683
davronkhamdamov <xamdamovdavron6@gmail.com>
2023-06-02 23:32:12 +0500

    profile drop dawn added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,10 +3,13 @@ import "./Header.css";
 import { Link, useLocation } from "react-router-dom";
 import { AiOutlineUser } from "react-icons/ai";
 import { BiLogIn } from "react-icons/bi";
+import { CgProfile } from "react-icons/cg";
+import { FiLogOut } from "react-icons/fi";
 
 const Header = () => {
   const location = useLocation();
   const [isLogin, setIslogin] = useState(true);
+  const [isActive, setIsActive] = useState(false);
   if (["/login"].includes(location.pathname)) return;
   return (
     <div className="header">
@@ -26,11 +29,30 @@ const Header = () => {
             </Link>
           </div>
         ) : (
-          <Link className="profile_img" to="/profile">
+          <div
+            className={isActive ? "profile_img active" : "profile_img"}
+            onClick={() => {
+              setIsActive(!isActive);
+            }}
+          >
             <img src="https://picsum.photos/60/60" alt="" />
-          </Link>
+            <div className="profile_model">
+              <Link to="profile">
+                <CgProfile />
+                Profile
+              </Link>
+              <Link to="login">
+                <FiLogOut />
+                Logout
+              </Link>
+            </div>
+          </div>
         )}
       </>
+      <div
+        className={isActive ? "visible active" : "visible"}
+        onClick={() => setIsActive(!isActive)}
+      />
     </div>
   );
 };
