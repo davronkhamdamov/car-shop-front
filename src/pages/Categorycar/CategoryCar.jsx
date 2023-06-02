e9a1a1d16df301c49e31f5625e274af3f53ef45e
davronkhamdamov <xamdamovdavron6@gmail.com>
2023-06-02 23:36:01 +0500

    category Car complate

---
+++ src/pages/Categorycar/CategoryCar.jsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from "react";
+import Card from "../../components/Card/Card";
+import "./CategoryCar.css";
+import { useParams } from "react-router-dom";
+
+const CategoryCar = () => {
+  const [data, setData] = useState([1]);
+  const { id } = useParams();
+  useEffect(() => {
+    fetch(process.env.REACT_APP_BASE_URL + "/model/owncars/" + id, {
+      headers: {
+        authorization: localStorage.getItem("token"),
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.statusCode === 401) {
+          window.location = "/login";
+        }
+        setData(data);
+      });
+  }, []);
+  return (
+    <div className="category_wrapper">
+      {data[0] ? (
+        <Card data={data} type={"car"} />
+      ) : (
+        <div style={{ textAlign: "center", width: "100%" }}>
+          <h2>Nothing found</h2>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default CategoryCar;
